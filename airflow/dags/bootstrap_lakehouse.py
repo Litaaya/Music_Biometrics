ffbdcaf9e9ca0db3157bfb1b23930ab8e71e8373
Litaaya <minhquans2810@gmail.com>
2026-07-08 19:50:59 +0700

    feat: initial created dags for lakehouse and pipeline

---
+++ airflow/dags/bootstrap_lakehouse.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    "owner": "QuanDo",
+    "depends_on_past": False,
+    "start_date": datetime(2026, 1, 1),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=2),
+}
+
+with DAG(
+    "01_bootstrap_lakehouse",
+    default_args=default_args,
+    description="Initialize Lakehouse base data and dbt staging models",
+    schedule_interval="@once",
+    catchup=False,
+    tags=["bootstrap", "lakehouse"],
+) as dag:
+
+    run_bootstrap_loader = BashOperator(
+        task_id="run_bootstrap_loader",
+        bash_command="python /opt/airflow/dbt_project/src/loader/main.py",
+    )
+
+    dbt_debug_check = BashOperator(
+        task_id="dbt_debug_check",
+        bash_command="cd /opt/airflow/dbt_project && dbt debug",
+    )
+
+    dbt_build_staging = BashOperator(
+        task_id="dbt_build_staging",
+        bash_command="cd /opt/airflow/dbt_project && dbt build --select path:models/staging",
+    )
+
+    run_bootstrap_loader >> dbt_debug_check >> dbt_build_staging
---
+++ airflow/dags/daily_biometric_pipeline.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    "owner": "QuanDo",
+    "depends_on_past": False,
+    "start_date": datetime(2026, 1, 1),
+    "retries": 2,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    "02_daily_biometric_pipeline",
+    default_args=default_args,
+    description="Daily ELT pipeline for biometric Silver and Gold models",
+    schedule="5 0 * * *",
+    catchup=False,
+    tags=['production', 'daily', 'marts']
+) as dag:
+
+    log_pipeline_start = BashOperator(
+        task_id="log_pipeline_start",
+        bash_command='echo "[AUDIT LOG] Starting daily biometric pipeline at $(date)"',
+    )
+
+    execute_dbt_build = BashOperator(
+        task_id="execute_dbt_build",
+        bash_command="cd /opt/airflow/dbt_project && dbt build",
+    )
+
+    log_pipeline_success = BashOperator(
+        task_id="log_pipeline_success",
+        bash_command='echo "[AUDIT LOG] Pipeline completed successfully. Gold tables are ready for Power BI."',
+    )
+
+    log_pipeline_start >> execute_dbt_build >> log_pipeline_success
